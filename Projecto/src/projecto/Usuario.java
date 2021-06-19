7c236271c5d3f449587a11df5dae02fb938249be
jodavera <Danielitto@192.168.200.6>
2021-06-19 02:17:20 -0500

    Josue Vera hizo la clase usuario

---
+++ Projecto/src/projecto/Usuario.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projecto;
+
+/**
+ *
+ * @author Danielitto
+ */
+public class Usuario {
+    private String usuario;
+    private String nombre;
+    private String correo;
+    private String us_tipo;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getUs_tipo() {
+        return us_tipo;
+    }
+
+    public void setUs_tipo(String us_tipo) {
+        this.us_tipo = us_tipo;
+    }
+
+ 
+ 
+}
