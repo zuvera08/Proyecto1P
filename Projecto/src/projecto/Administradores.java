d60073509b71e96339ab37e2f5491635f02dca21
Jodavera <Danielitto@192.168.200.6>
2021-07-11 10:57:14 -0500

    Josue Vera creo metodo opciones para administrador

--- Projecto/src/projecto/Administradores.java
+++ Projecto/src/projecto/Administradores.java
@@ -5,16 +5,46 @@
  */
 package projecto;
 
+import java.util.Scanner;
+
 /**
  *
  * @author PC
  */
 public class Administradores extends Usuario {
-
+    Scanner sc = new Scanner(System.in);
     public Administradores(String usuario, String nombre, String cedula, String correo, String contrasenia) {
         super(usuario, nombre, cedula, correo, contrasenia);
     }
+public void salir() {
+    
+    }
 
-    
-    
+    public void opcionesAdministrador(BaseDatos bd) {
+        String opcion = "";
+        do {
+            System.out.println("Elija una opcion :");
+            System.out.println("REGISTRAR PROPIEDAD (1):");
+            System.out.println("REGISTRAR AGENTE (2):");
+            System.out.println("VER REPORTE AGENTE Y VENTAS (3):");
+            System.out.println("Salir(4)");
+            opcion = sc.next();
+            sc.nextLine();
+            switch (opcion) {
+                case "1":
+                    registrarPropiedad(bd);
+                    break;
+                case "2":
+                    registrarAgente(bd);
+                    break;
+                case "3":
+                    verReporteDeVentasYContactos(bd);
+                    break;
+            }
+
+        } while (opcion != "4");
+        salir();
+        sc.close();
+
+    }
 }
