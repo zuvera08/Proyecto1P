523aae2efb968b99ccc0b017618a1ec65e2d6a5c
Jodavera <Danielitto@192.168.200.6>
2021-07-11 10:54:30 -0500

    Josue Vera creo la base de datos

---
+++ Projecto/src/projecto/BaseDatos.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projecto;
+import java.util.ArrayList;
+import  Propiedades.Propiedades;
+/**
+ *
+ * @author Danielitto
+ */
+public class BaseDatos {
+
+
+/**
+ *
+ * @author Danielitto
+ */
+    private ArrayList<Usuario> usuarios;
+    private ArrayList<Consultas> consultas;
+    private ArrayList<Propiedades> propiedades;
+    private ArrayList<Venta> ventas;
+
+    
+    public ArrayList<Venta> getVentas() {
+        return ventas;
+    }
+    
+
+    public ArrayList<Propiedades> getPropiedades() {
+        return propiedades;
+    }
+    
+    public ArrayList<Consultas> getConsulta() {
+        return consultas;
+    }
+    
+    public ArrayList<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    
+}
+    
+
